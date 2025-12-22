9c68e95be27b99c12825ccd39c45316f6a8809c3
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-12-21 21:44:33 -0300

    add aula5

---
+++ aula5-count.sql
@@ -0,0 +1,20 @@
+SELECT count(*)
+FROM person.Person;
+
+SELECT count(title)
+FROM person.Person;
+
+SELECT count(DISTINCT title)
+FROM person.Person;
+
+-- DESAFIO 1
+SELECT count(*)
+FROM production.Product
+
+-- DESAFIO 2
+SELECT count(Size)
+FROM production.Product
+
+-- DESAFIO 3
+SELECT count(DISTINCT Size)
+FROM production.Product
